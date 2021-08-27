4a53c48c43a1711216b4eb1fe0c6af5e3dde688e
Guillaume PETIT <gpetit@e1r12p18.42.fr>
2021-08-27 14:35:24 +0200

    multithreading now launching simultaniously

--- philo/srcs/tamagochi_actions.c
+++ philo/srcs/tamagochi_actions.c
@@ -12,53 +12,65 @@
 
 #include "philo.h"
 
-int	forks(int action, int current_ph, t_datas *data)
+int	set_left_philo(int *index, int current_ph, t_datas *data)
+{
+	int left;
+
+	*index = current_ph - 1;
+	if (*index == 0)
+		left = data->nb - 1;
+	else
+		left = *index - 1;
+	return (left);
+}
+
+int	drop_forks(int current_ph, t_datas *data)
 {
 	int left;
 	int	index;
 	t_philo *ph;
 
-	index = current_ph - 1;
-	if (index == 0)
-		left = data->nb - 1;
-	else
-		left = index - 1;
 	ph = data->philo;
-	if (pthread_mutex_lock(&ph[index].mutex))
-		return (ERROR);
-	ph[index].fork = action;
-	if (!action)
-		printf("%ld Philo_%d has taken a fork\n", get_time_elapsed(data), current_ph);
-	if (pthread_mutex_unlock(&ph[index].mutex))
-		return (ERROR);
-	if (pthread_mutex_lock(&ph[left].mutex))
-		return (ERROR);
-	ph[left].fork = action;
-	if (!action)
-		printf("%ld Philo_%d has taken a fork\n", get_time_elapsed(data), current_ph);
-	if (pthread_mutex_unlock(&ph[left].mutex))
-		return (ERROR);
+	left = set_left_philo(&index, current_ph, data);
+	ph[index].fork = 1;
+	ph[left].fork = 1;
+	pthread_mutex_unlock(&ph[index].mutex); //CONTROLLER MUTEX
+	pthread_mutex_unlock(&ph[left].mutex); //CONTROLLER MUTEX
+	return (SUCCESS);
+}
+
+int	take_forks(int current_ph, t_datas *data)
+{
+	int left;
+	int	index;
+	t_philo *ph;
+
+	ph = data->philo;
+	left = set_left_philo(&index, current_ph, data);
+	pthread_mutex_lock(&ph[index].mutex); //CONTROLLER MUTEX
+	pthread_mutex_lock(&ph[left].mutex); //CONTROLLER MUTEX
+	ph[index].fork = 0;
+	printf("%ld Philo_%d has taken a fork\n", get_time_elapsed(data), current_ph); //CONTROLLER RETOUR GET_TIME
+	ph[left].fork = 0;
+	printf("%ld Philo_%d has taken a fork\n", get_time_elapsed(data), current_ph); //CONTROLLER RETOUR GET_TIME
 	return (SUCCESS);
 }
 
 void	start_eating(int current_ph, t_datas *data)
 {
-	printf("%ld Philo_%d is eating\n", get_time_elapsed(data), current_ph);
+	printf("%ld Philo_%d is eating\n", get_time_elapsed(data), current_ph); //CONTROLLER RETOUR GET_TIME
 	usleep(data->tte * 1000);
-	data->philo[current_ph - 1].last_dinner = get_time_elapsed(data);
+	data->philo[current_ph - 1].last_dinner = get_time_elapsed(data); //CONTROLLER RETOUR GET_TIME
 }
 
 int	tamagochi_philo(int thread_nb, t_datas *data)
 {
-/* 	if (thread_nb % 2 == 0)
-		usleep(data->tte / 10); */
- 	if (forks(0, thread_nb, data)) //FORK PICKING
+ 	if (take_forks(thread_nb, data))
 		return (ERROR);
 	start_eating(thread_nb, data);
-	if (forks(1, thread_nb, data)) //FORK DROPPING
+	if (drop_forks(thread_nb, data))
 		return (ERROR);
-/* 	drop_fork;
-	start_sleeping;
+/*	start_sleeping;
 	start_thinking; */
 	return(SUCCESS);
 }
